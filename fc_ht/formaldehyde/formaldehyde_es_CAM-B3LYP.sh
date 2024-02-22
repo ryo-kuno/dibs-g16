6824ee1daf3d53671bf31f7cdf8daf4c59398e9c
Ryo HPC <haveaniceday0814@outlook.jp>
2024-02-22 11:00:28 +0000

    update Feb 22, CAM-B3LYP calculations

--- fc_ht/formaldehyde/formaldehyde_es_CAM-B3LYP.sh
+++ fc_ht/formaldehyde/formaldehyde_es_CAM-B3LYP.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
-#PBS -l walltime=01:00:00
+#PBS -l walltime=00:10:00
 #PBS -l select=1:ncpus=32:mem=128GB
 
 module load "gaussian/g16-c01-avx2" 
+cp "/rds/general/user/rk820/home/dibs-g16/fc_ht/formaldehyde/./formaldehyde_es_CAM-B3LYP.chk" ./
 cp "/rds/general/user/rk820/home/dibs-g16/fc_ht/formaldehyde/./formaldehyde_es_CAM-B3LYP.gjf" ./
 
 #convert old checkpoints to latest (i.e. for g03 checkpoints generated before ~Dec 2009)
