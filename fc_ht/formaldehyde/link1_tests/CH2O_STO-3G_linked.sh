0174ee691c1c3cbeb1ac8032579a17d8acdf8764
Ryo HPC <haveaniceday0814@outlook.jp>
2024-02-22 18:19:09 +0000

    Link1 test worked!

--- fc_ht/formaldehyde/link1_tests/CH2O_STO-3G_linked.sh
+++ fc_ht/formaldehyde/link1_tests/CH2O_STO-3G_linked.sh
@@ -3,7 +3,7 @@
 #PBS -l select=1:ncpus=1:mem=8GB
 
 module load "gaussian/g16-c01-avx2" 
-cp "/rds/general/user/rk820/home/dibs-g16/fc_ht/formaldehyde/./CH2O_STO-3G_linked.gjf" ./
+cp "/rds/general/user/rk820/home/dibs-g16/fc_ht/formaldehyde/link1_tests/CH2O_STO-3G_linked.gjf" ./
 
 #convert old checkpoints to latest (i.e. for g03 checkpoints generated before ~Dec 2009)
 #c8609 "CH2O_STO-3G_linked.chk"
@@ -11,7 +11,7 @@ cp "/rds/general/user/rk820/home/dibs-g16/fc_ht/formaldehyde/./CH2O_STO-3G_linke
 # no more pbsexec... running directly.
 g16 CH2O_STO-3G_linked.gjf
 
-cp *.log  /rds/general/user/rk820/home/dibs-g16/fc_ht/formaldehyde/./ 
-cp *.chk  /rds/general/user/rk820/home/dibs-g16/fc_ht/formaldehyde/./
+cp *.log  /rds/general/user/rk820/home/dibs-g16/fc_ht/formaldehyde/link1_tests/./ 
+cp *.chk  /rds/general/user/rk820/home/dibs-g16/fc_ht/formaldehyde/link1_tests/./
 
 echo "For us, there is only the trying. The rest is not our business. ~T.S.Eliot"
