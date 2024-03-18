dca5f01ead2e93656afcec83f6b1654b7974f094
Ryo HPC <haveaniceday0814@outlook.jp>
2024-03-18 11:44:14 +0000

    update March 18th 2024

--- fc_ht/project/Perylene_linked2.sh
+++ fc_ht/project/Perylene_linked2.sh
@@ -1,15 +1,15 @@
 #!/bin/sh
-#PBS -l walltime=04:00:00
-#PBS -l select=1:ncpus=128:mem=919GB
+#PBS -l walltime=05:00:00
+#PBS -l select=1:ncpus=128:mem=912GB
 
 module load "gaussian/g16-c01-avx2" 
-cp "/rds/general/user/rk820/home/dibs-g16/fc_ht/project/./Perylene_linked1.gjf" ./
+cp "/rds/general/user/rk820/home/dibs-g16/fc_ht/project/./Perylene_linked2.gjf" ./
 
 #convert old checkpoints to latest (i.e. for g03 checkpoints generated before ~Dec 2009)
-#c8609 "Perylene_linked1.chk"
+#c8609 "Perylene_linked2.chk"
 
 # no more pbsexec... running directly.
-g16 Perylene_linked1.gjf
+g16 Perylene_linked2.gjf
 
 cp *.log  /rds/general/user/rk820/home/dibs-g16/fc_ht/project/./ 
 cp *.chk  /rds/general/user/rk820/home/dibs-g16/fc_ht/project/./
