e01e03a818c58e7b358494f0cfa3b9242b7f015d
Ryo HPC <haveaniceday0814@outlook.jp>
2024-03-27 11:30:34 +0000

    update March 27th

---
+++ fc_ht/project/Phenanthrene/Phenanthrene_n_linked1.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+#PBS -l walltime=05:00:00
+#PBS -l select=1:ncpus=32:mem=256GB
+
+module load "gaussian/g16-c01-avx2" 
+cp "/rds/general/user/rk820/home/dibs-g16/fc_ht/project/Phenanthrene/./Phenanthrene_n_linked1.gjf" ./
+
+#convert old checkpoints to latest (i.e. for g03 checkpoints generated before ~Dec 2009)
+#c8609 "Phenanthrene_n_linked1.chk"
+
+# no more pbsexec... running directly.
+g16 Phenanthrene_n_linked1.gjf
+
+cp *.log  /rds/general/user/rk820/home/dibs-g16/fc_ht/project/Phenanthrene/./ 
+cp *.chk  /rds/general/user/rk820/home/dibs-g16/fc_ht/project/Phenanthrene/./
+
+echo "For us, there is only the trying. The rest is not our business. ~T.S.Eliot"
