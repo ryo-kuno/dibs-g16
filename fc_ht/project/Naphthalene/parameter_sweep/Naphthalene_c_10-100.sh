15537ae4d7ec84922f4c3fcdc21f162e6006c514
Ryo HPC <haveaniceday0814@outlook.jp>
2024-04-05 11:32:11 +0100

    April 5th update

---
+++ fc_ht/project/Naphthalene/parameter_sweep/Naphthalene_c_10-100.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+#PBS -l walltime=05:00:00
+#PBS -l select=1:ncpus=32:mem=256GB
+
+module load "gaussian/g16-c01-avx2" 
+cp "/rds/general/user/rk820/home/dibs-g16/fc_ht/project/Naphthalene/parameter_sweep/./Naphthalene_c_10-100.gjf" ./
+
+#convert old checkpoints to latest (i.e. for g03 checkpoints generated before ~Dec 2009)
+#c8609 "Naphthalene_c_10-100.chk"
+
+# no more pbsexec... running directly.
+g16 Naphthalene_c_10-100.gjf
+
+cp *.log  /rds/general/user/rk820/home/dibs-g16/fc_ht/project/Naphthalene/parameter_sweep/./ 
+cp *.chk  /rds/general/user/rk820/home/dibs-g16/fc_ht/project/Naphthalene/parameter_sweep/./
+
+echo "For us, there is only the trying. The rest is not our business. ~T.S.Eliot"
