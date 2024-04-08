f65220ebc826567e4999416347c17583e0e90f0f
Ryo HPC <haveaniceday0814@outlook.jp>
2024-04-08 12:15:52 +0100

    April 8th update

---
+++ fc_ht/project/Naphthalene/convergence_test/Naphthalene_n.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+#PBS -l walltime=05:00:00
+#PBS -l select=1:ncpus=32:mem=256GB
+
+module load "gaussian/g16-c01-avx2" 
+cp "/rds/general/user/rk820/home/dibs-g16/fc_ht/project/Naphthalene/convergence_test/./Naphthalene_n.gjf" ./
+
+#convert old checkpoints to latest (i.e. for g03 checkpoints generated before ~Dec 2009)
+#c8609 "Naphthalene_n.chk"
+
+# no more pbsexec... running directly.
+g16 Naphthalene_n.gjf
+
+cp *.log  /rds/general/user/rk820/home/dibs-g16/fc_ht/project/Naphthalene/convergence_test/./ 
+cp *.chk  /rds/general/user/rk820/home/dibs-g16/fc_ht/project/Naphthalene/convergence_test/./
+
+echo "For us, there is only the trying. The rest is not our business. ~T.S.Eliot"
