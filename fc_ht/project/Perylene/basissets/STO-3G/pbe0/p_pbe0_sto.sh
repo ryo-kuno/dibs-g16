06efd9bd481b7f39aac46f6258e926fd037fb735
Ryo HPC <haveaniceday0814@outlook.jp>
2024-04-22 04:15:19 +0100

    updated spectra with narrower peaks

--- fc_ht/project/Perylene/basissets/STO-3G/pbe0/p_pbe0_sto.sh
+++ fc_ht/project/Perylene/basissets/STO-3G/pbe0/p_pbe0_sto.sh
@@ -3,7 +3,7 @@
 #PBS -l select=1:ncpus=16:mem=128GB
 
 module load "gaussian/g16-c01-avx2" 
-cp "/rds/general/user/rk820/home/dibs-g16/fc_ht/project/Perylene/basissets/STO-3G/./p_pbe0_sto.gjf" ./
+cp "/rds/general/user/rk820/home/dibs-g16/fc_ht/project/Perylene/basissets/STO-3G/pbe0/./p_pbe0_sto.gjf" ./
 
 #convert old checkpoints to latest (i.e. for g03 checkpoints generated before ~Dec 2009)
 #c8609 "p_pbe0_sto.chk"
