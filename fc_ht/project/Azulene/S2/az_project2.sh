4461b077ffaa74342d71f5cf3ebd682a5bb6d978
Ryo HPC <haveaniceday0814@outlook.jp>
2024-05-06 08:36:31 +0100

    azulene updates

---
+++ fc_ht/project/Azulene/S2/az_project2.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+#PBS -l walltime=04:00:00
+#PBS -l select=1:ncpus=32:mem=264GB
+
+module load "gaussian/g16-c01-avx2" 
+cp "/rds/general/user/rk820/home/dibs-g16/fc_ht/project/Azulene/S2/./az_project2.gjf" ./
+
+#convert old checkpoints to latest (i.e. for g03 checkpoints generated before ~Dec 2009)
+#c8609 "az_project2.chk"
+
+# no more pbsexec... running directly.
+g16 az_project2.gjf
+
+cp *.log  /rds/general/user/rk820/home/dibs-g16/fc_ht/project/Azulene/S2/./ 
+cp *.chk  /rds/general/user/rk820/home/dibs-g16/fc_ht/project/Azulene/S2/./
+
+echo "For us, there is only the trying. The rest is not our business. ~T.S.Eliot"
